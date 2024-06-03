b57c5fec196a69850d0616fc1fe0e7b0421f4a2c
palli.hemanth <palli.hemanth@MNGNET587944L.ad.infosys.com>
2024-06-03 12:17:14 +0530

    First Commit

---
+++ Project/src/main/java/Project_1/Project/App.java
@@ -0,0 +1,13 @@
+package Project_1.Project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Project/src/test/java/Project_1/Project/Sample1.java
@@ -0,0 +1,11 @@
+package Project_1.Project;
+
+public class Sample1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World");
+
+	}
+
+}
